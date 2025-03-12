82d96ceed960b2346239e30c66c4f45315401a31
kevrtt <kevinricotorres@gmail.com>
2025-03-11 19:05:09 -0500

    Añadir funciones de estadísticas descriptivas

---
+++ analisis/estadisticas.py
@@ -0,0 +1,14 @@
+# analisis/estadisticas.py
+
+import numpy as np
+
+# Datos de ejemplo
+data = [10, 20, 30, 40, 50]
+
+# Corrección: uso correcto de np.mean
+promedio = np.mean(data)
+
+print("El promedio es:", promedio)
+
+
+
